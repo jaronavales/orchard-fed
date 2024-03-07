bbfec8284f634bbb025c5171235790e0adaca348
JARONAVALES-DESKTOP\Admin <jaronavales@gmail.com>
2024-03-08 00:26:32 +0800

    add modal, console log click event, dynamic image click to modal view

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 // Import our custom CSS
 import '../scss/styles.scss';
 
-// Import only the Bootstrap components we need
-import { Modal } from 'bootstrap';
+$(".item").click(function() {
+    const imagePath = $(this).find(".item-image").attr("src");
+    console.log("image path for modal =>", imagePath);
+    $("#modal-image").attr("src", imagePath);
+});
+
+$('.card-body .link, .card-body .card-title').click(function(event) {
+    // Prevent the default link action
+    event.preventDefault();
+
+    // Navigate up to the closest .card-body and then find the .card-title h6 to get its text
+    const cardTitle = $(this).closest('.card-body').find('.card-title h6').text();
+
+    // Log the card title
+    console.log("All the latest from AEG => Blog title:", cardTitle);
+});
+
 
